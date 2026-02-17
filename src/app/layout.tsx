49ac310ba3902d9c7aba2b9fe5ea5663103d7099
Fathurrohman Solehudin <52761218+fasoleh7@users.noreply.github.com>
2026-02-18 03:28:37 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,8 +8,8 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Suspense } from "react";
 
 export const metadata: Metadata = {
-  title: "SekaiDrama - Streaming Drama Pendek",
-  description: "Nonton drama pendek gratis dan tanpa iklan di SekaiDrama.",
+  title: "Rohmnn Drama - Streaming Drama Pendek",
+  description: "Nonton drama pendek gratis dan tanpa iklan di Rohmnn Drama.",
 };
 
 export default function RootLayout({
